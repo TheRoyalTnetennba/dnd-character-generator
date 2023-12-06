be038163c4147aadf1e66f62a7098697d8cbe243
TheRoyalTnetennba <gpaye8@gmail.com>
2023-12-06 10:07:15 -0700

    add create-react-app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Welcome to the dnd character generator
+      </header>
+      <button>Generate!</button>
+    </div>
+  );
+}
+
+export default App;
